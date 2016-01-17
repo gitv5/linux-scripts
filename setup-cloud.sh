7adde5cda85a67c5c4538a8d9d66261d11887d9e
Daniel H. Hemmingsen <dhh@v5.dk>
2016-01-17 11:45:48 +0100

    Tilføjet lidt comments omkring brug og fikset et par enkelte linjer

--- setup-cloud.sh
+++ setup-cloud.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Dette script er beregnet til at blive kørt som root!
+# Det er meningen at det kun skal køres en enkel gang for at komme hurtigt
+# igang med sin nye v5 Cloud Server.
+# Efter det kan man blot fjerne scriptet igen fra systemet.
+# $ wget https://raw.githubusercontent.com/gitv5/linux-scripts/master/setup-cloud.sh
+# $ chmod +x setup-cloud.sh
+# $ ./setup-cloud.sh
+# $ rm setup-cloud.sh
 
 function ask() {
 	read -p "$1 (j/N): "
@@ -8,14 +16,12 @@ function ask() {
 	esac
 }
 
-if [[ "yes" == $(ask "Vil du opdatere systemet?") ]]
-then
+if [[ "yes" == $(ask "Vil du opdatere systemet?") ]]; then
 	apt-get update && apt-get upgrade
 fi
 
 if [[ "yes" == $(ask "Vil du lave en ny bruger til dig selv?") ]]; then
-	read -p "$1 Brugernavn: "
-	adduser $REPLY && gpasswd -a $REPLY sudo
+	read -p "$1 Brugernavn: " && adduser $REPLY && gpasswd -a $REPLY sudo
 fi
 
 if [[ "yes" == $(ask "Slå root fra?") ]]; then
